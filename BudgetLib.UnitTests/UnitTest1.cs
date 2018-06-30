738587f325883e2e751a94c6ca3892012b2f322f
oomusou <oomusou@gmail.com>
2018-07-01 00:53:01 +0800

    Test Case 2 : 日期區間在 Budget 內 (紅燈)

--- BudgetLib.UnitTests/UnitTest1.cs
+++ BudgetLib.UnitTests/UnitTest1.cs
@@ -22,6 +22,16 @@ namespace BudgetLib.UnitTests
             AmountShouldBe(0m, new DateTime(2018, 7, 15), new DateTime(2018, 7, 15));
         }
 
+        [TestMethod]
+        public void 日期區間在Budget內()
+        {
+            _budgetRepository.GetAll().Returns(new List<Budget>
+            {
+                new Budget {YearMonth = "201807", Amount = 31m}
+            });
+            AmountShouldBe(1m, new DateTime(2018, 7, 15), new DateTime(2018, 7, 15));
+        }
+
         private void AmountShouldBe(decimal expected, DateTime start, DateTime end)
         {
             var totalAmount = _accounting.TotalAmount(start, end);
