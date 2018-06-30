1ea6edb0908c81d2c59e2cf244ef4acd9a414435
oomusou <oomusou@gmail.com>
2018-07-01 00:09:52 +0800

    Test Case 1 : 重構測試

--- BudgetLib.UnitTests/UnitTest1.cs
+++ BudgetLib.UnitTests/UnitTest1.cs
@@ -7,15 +7,25 @@ namespace BudgetLib.UnitTests
     [TestClass]
     public class UnitTest1
     {
+        private readonly IBudgetRepository _budgetRepository = Substitute.For<IBudgetRepository>();
+        private Accounting _accounting;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _accounting = new Accounting(_budgetRepository);
+        }
+
         [TestMethod]
         public void 日期區間無Budget()
         {
-            var budgetRepository = Substitute.For<IBudgetRepository>();
-            var accounting = new Accounting(budgetRepository);
-            var start = new DateTime(2018, 7, 15);
-            var end = new DateTime(2018, 7, 15);
-            var totalAmount = accounting.TotalAmount(start, end);
-            Assert.AreEqual(0m, totalAmount);
+            AmountShouldBe(0m, new DateTime(2018, 7, 15), new DateTime(2018, 7, 15));
+        }
+
+        private void AmountShouldBe(decimal expected, DateTime start, DateTime end)
+        {
+            var totalAmount = _accounting.TotalAmount(start, end);
+            Assert.AreEqual(expected, totalAmount);
         }
     }
 }
